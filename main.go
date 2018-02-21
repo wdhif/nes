1301fb3b341a806670881574268ef854bd8501db
Wassim DHIF <wassimdhif@gmail.com>
2018-02-21 18:10:26 +0100

    Add usage details

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ import (
 func main() {
 	if len(os.Args) > 1 {
 		path := os.Args[1]
-		fmt.Println(path)
+		fmt.Println("Path to the rom:", path)
 		nes.Loader(path)
 	} else {
-		fmt.Println("Usage go run main.go nestest.nes")
+		fmt.Println("Usage go run main.go roms/nestest.nes")
 	}
 }
