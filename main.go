ed9b9edb56f6ecf184564b5da89c03d081ffd00b
Wassim DHIF <wassimdhif@gmail.com>
2018-05-03 21:52:04 +0200

    Add Banner
    Signed-off-by: Wassim DHIF <wassimdhif@gmail.com>

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/wdhif/nes/nes"
-	"log"
+)
+
+const (
+	BANNER = `
+   _  __________
+  / |/ / __/ __/
+ /    / _/_\ \  
+/_/|_/___/___/  
+                
+A Nintendo Entertainment System emulator in Go
+ Version: %s
+
+`
 )
 
 func main() {
+	fmt.Fprint(os.Stdout, fmt.Sprintf(BANNER, "0.1.0"))
 	if len(os.Args) > 1 {
 		path := os.Args[1]
 		fmt.Println("Path to the rom:", path)
