363787ad55bff99cf4e62c4429cea403ef3c040b
Wassim Dhif <wassimdhif@gmail.com>
2018-06-13 21:24:13 +0200

    Add specific tests without GUI

---
+++ main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/wdhif/nes/nes"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Fprint(os.Stdout, fmt.Sprintf(BANNER, "0.1.0"))
+	if len(os.Args) > 1 {
+		path := os.Args[1]
+		fmt.Println("Path to the rom:", path)
+		_, err := nes.Loader(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Println("Usage go run main.go roms/nestest.nes")
+	}
+}
