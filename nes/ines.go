5f6d90deb4706e797ccf31f2756a6146f612b3d9
Wassim DHIF <wassimdhif@gmail.com>
2023-05-18 21:25:36 +0200

    [ines] Add NES 2.0 format check

--- nes/ines.go
+++ nes/ines.go
@@ -8,21 +8,21 @@ import (
 	"os"
 )
 
-// iNES Magic Number is "NES" followed by MS-DOS end-of-file
-// Hexadecimal, stored in Little endian
+// iNES Magic Number is "NES" followed by MS-DOS end-of-file.
+// Hexadecimal, stored in Little endian.
 const iNESMagicNumber = 0x1a53454e
 
 type iNESFileHeader struct {
 	MagicNumber             uint32  // iNES Magic Number (32 bits / bytes 0-3)
 	ProgramROMBanksNumber   byte    // Number of 16 KB PRG-ROM banks (8 bits / byte 4)
 	CharacterROMBanksNumber byte    // Number of 8 KB CHR-ROM banks (8 bits / byte 5)
-	ControlByte1            byte    // ROM Control Byte 1 (8 bits / byte 6)
-	ControlByte2            byte    // ROM Control Byte 2 (8 bits / byte 7)
+	ControlByte1            byte    // ROM Control Byte 1 (Flag 6) (8 bits / byte 6)
+	ControlByte2            byte    // ROM Control Byte 2 (Flag 7) (8 bits / byte 7)
 	RAM                     byte    // Number of 8 KB RAM banks (8 bits / byte 8)
 	_                       [7]byte // Unused, should all be 0 (8 bits / bytes 9-15)
 }
 
-// Loader reads an iNES file and return a ROM
+// Loader reads an iNES file and return a ROM.
 func Loader(romPath string) (*Rom, error) {
 
 	fmt.Print("Loading NES ROM... ")
@@ -32,14 +32,20 @@ func Loader(romPath string) (*Rom, error) {
 	}
 	defer file.Close()
 
+	// Read rom header.
 	romHeader := iNESFileHeader{}
 	binary.Read(file, binary.LittleEndian, &romHeader)
 
 	// Check valid Magic Number against rom header
 	if romHeader.MagicNumber != iNESMagicNumber {
-		return nil, errors.New("file is not a valid ROM: Invalid Magic Number")
+		return nil, errors.New("file is not a valid ROM: Invalid Magic Number.")
 	}
 	fmt.Println("ROM is valid")
+
+	// Check iNES format (NES 2.0)
+	if romHeader.ControlByte2&0x0c == 0x08 {
+		return nil, errors.New("NES 2.0 format is not implemented.")
+	}
 
 	// ProgramRom
 	ProgramRom := make([]byte, int(romHeader.ProgramROMBanksNumber)*16384)
